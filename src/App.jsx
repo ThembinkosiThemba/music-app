864a10c2a183c4a4737b01665f7b08a276efb1ce
ThembinkosiThemba <thembinkosimkhonta01@gmail.com>
2024-02-05 14:08:23 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-gray-800 flex min-h-screen items-center justify-center">
+      <h1 className="text-white text-5xl">Welcome to the music app!</h1>
+    </div>
+  );
+};
+
+export default App;
